4126d2026196b1776649cfc30c875d210ccc69da
migueldossantos1 <miguelhcs.2005@gmail.com>
2025-02-26 19:51:56 +0000

    adicionei um ficheiro de texto para ir testando o algoritmo, e implementei o qsort no dictionary

--- main.c
+++ main.c
@@ -3,14 +3,19 @@
 #include <string.h>
 #define MAX_WORD 100 /*tamanho máximo de uma palavra; ajustável*/
 
+/*função que compara dois argumentos sem ter em conta maiúsculas*/
+int compare(const void *arg1, const void *arg2){
+    return strcasecmp(*(const char **)arg1, *(const char **)arg2);
+}
+
 void print_help(){
-    printf("-h      mostra a ajuda para o utilizador e termina\n");
-    printf("-i      filename nome do ficheiro de entrada, em alternativa a stdin\n");
-    printf("-o      filename nome do ficheiro de saída, em alternativa a stdout\n");
-    printf("-d      filename nome do ficheiro de dicionário a usar\n");
-    printf("-a      nn o número máximo de alternativas a mostrar com cada erro ortográfico deve ser nn\n");
-    printf("-n      nn o número máximo de diferenças a considerar nos erros ortográficos deve ser nn\n");
-    printf("-m      nn o modo de funcionamento do programa deve ser nn\n");
+    printf("-h              mostra a ajuda para o utilizador e termina\n");
+    printf("-i filename     nome do ficheiro de entrada, em alternativa a stdin\n");
+    printf("-o filename     nome do ficheiro de saída, em alternativa a stdout\n");
+    printf("-d filename     nome do ficheiro de dicionário a usar\n");
+    printf("-a nn           o número máximo de alternativas a mostrar com cada erro ortográfico deve ser nn\n");
+    printf("-n nn           o número máximo de diferenças a considerar nos erros ortográficos deve ser nn\n");
+    printf("-m nn           o modo de funcionamento do programa deve ser nn\n");
 }
 
 int main(int argc, char *argv[]){
@@ -75,6 +80,21 @@ int main(int argc, char *argv[]){
 
         counter++;
     }
+    /*
+    for(int i = 0; i < 10; i++){
+        printf("%s\n", dictionary[i]);
+    }
+    */
+
+    qsort(dictionary, counter, sizeof(char *), compare);
+
+    /*
+    for(int i = 0; i < 100; i++){
+        printf("%s\n", dictionary[i]);
+    }
+    */
+
+    
 
     fclose(file);
 
