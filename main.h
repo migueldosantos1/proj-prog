15e9581588d9bf45ba82e6fe9b03139d38252e3d
migueldossantos1 <miguelhcs.2005@gmail.com>
2025-03-28 19:09:51 +0000

    slavery is a choice

--- main.h
+++ main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+ 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -8,38 +8,38 @@
 #include <ctype.h>
 #define MAX_WORD 100
 #define MAX_LINE 100
-
+ 
 typedef struct{
     char *word;
     int differences;
 } suggestion_data;
-
+ 
 bool output(int argc, char *argv[]);
-
+ 
 int compare_suggestions(const void *a, const void *b);
-
+ 
 int compare(const void *arg1, const void *arg2);
-
+ 
 int binary_search(char *word, char **dictionary, int size);
-
+ 
 void remove_newline(char *line);
-
+ 
 void clean_word(char *word);
-
+ 
 void print_help();
-
+ 
 void split(char *word, char **dictionary, int counter, suggestion_data *list, int *found, int maxdiffs);
-
-char* calculate_differences(char *token, char *word, int kanye);
-
-//int calculate_differences_reverse(char *token, char *word);
-
+ 
+int calculate_differences(char *token, char *word);
+ 
+int calculate_differences_reverse(char *token, char *word);
+ 
 void suggestions(int counter, int alt, char *token, char **dictionary, int maxdiffs, int argc, char *argv[], FILE *output_file);
-
+ 
 void mode1(FILE *input_file, FILE *output_file, char **dictionary, int counter, int argc, char *argv[]);
-
+ 
 void mode2(FILE *input_file, FILE *output_file, char **dictionary, int counter, int argc, char *argv[], int alt, char *word, int diffs);
-
-//void mode3(FILE *input_file, FILE *output_file, char **dictionary, int counter, int argc, char *argv[], int alt, int diffs);
+ 
+void mode3(FILE *input_file, FILE *output_file, char **dictionary, int counter, int argc, char *argv[], int alt, int diffs);
 
 #endif
